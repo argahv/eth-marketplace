22325cc673bba5007af861e8839d65c1456af969
Raghav Kattel <arg.ahv7@gmail.com>
2022-10-18 09:12:43 +0545

    deployment, ui/ux and toast

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,6 @@
+import { ToastContainer } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
 import "@styles/globals.css";
 
 const Noop = ({ children }) => <>{children}</>;
@@ -7,6 +10,7 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <Layout>
+      <ToastContainer />
       <Component {...pageProps} />
     </Layout>
   );
